b99566121a7768af67c8aeab900e3685177ca6b5
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-09-26 14:24:55 -0300

    add migrations e upload de arquivo

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middleware/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'ok' });
@@ -13,5 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
