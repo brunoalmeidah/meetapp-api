52d053839a259c751d3dbd64396491137d060096
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-09-30 23:27:22 -0300

    add inscrição de meetups

---
+++ src/app/controllers/SubscriptionController.js
@@ -0,0 +1,71 @@
+import * as Yup from 'yup';
+import { Op } from 'sequelize';
+import { startOfHour, endOfHour } from 'date-fns';
+import Subscription from '../models/Subscription';
+import Meetup from '../models/Meetup';
+
+class SubscriptionController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      meetup_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const user_id = req.userId;
+    const { meetup_id } = req.body;
+
+    const meetup = await Meetup.findByPk(meetup_id);
+
+    console.log(meetup.user_id === user_id || meetup.done);
+
+    if (meetup.user_id === user_id || meetup.done) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot subscribe for this meetup' });
+    }
+
+    const sameSubscription = await Subscription.findOne({
+      where: {
+        user_id,
+        meetup_id,
+      },
+    });
+
+    if (sameSubscription) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot subscribe for this meetup again' });
+    }
+
+    const haveMeetupInSameHour = await Subscription.findOne({
+      where: { user_id },
+      include: [
+        {
+          model: Meetup,
+          as: 'meetup',
+          where: {
+            date: {
+              [Op.between]: [startOfHour(meetup.date), endOfHour(meetup.date)],
+            },
+          },
+        },
+      ],
+    });
+
+    if (haveMeetupInSameHour) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot subscribe for this meetup.' });
+    }
+    const subscription = await Subscription.create({
+      user_id,
+      meetup_id,
+    });
+
+    return res.json(subscription);
+  }
+}
+
+export default new SubscriptionController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middleware/auth';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,5 +26,7 @@ routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
 routes.get('/meetups', MeetupController.index);
+
+routes.post('/subscription', SubscriptionController.store);
 
 export default routes;
